c47687c125bb5aa22bf389718e077aaf415ba0ec
Yan-Yi Li <liyanyi@gmail.com>
2016-11-07 17:29:19 +0000

    Confirm person is valid in person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Person, type: :model do
     Person.new(first_name: 'Alice', last_name: 'Smith')
   end
 
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
   it 'is invalid without a first name' do
     person.first_name = nil
     expect(person).not_to be_valid
